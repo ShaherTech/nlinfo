fc6158007e1871471e7742bda9aaa9aba5c92dd4
Shaher <y.saeed2004@gmail.com>
2025-04-17 20:22:41 +0200

    Add files via upload

---
+++ dutch_words_levels.js
@@ -0,0 +1,33 @@
+// كلمات هولندية لـ 15 مستوى (كل مستوى مصفوفة مستقلة، كلمات غير مكررة بين المستويات)
+const dutchLevels = [
+  // مستوى 1: سهل
+  ["huis", "boom", "fiets", "boek", "vis", "deur", "kat", "zon"],
+  // مستوى 2
+  ["stoel", "tafel", "raam", "auto", "weg", "jas", "kop", "schoen"],
+  // مستوى 3
+  ["lamp", "plant", "bord", "glas", "pen", "school", "klas", "stad"],
+  // مستوى 4
+  ["vogel", "brug", "toren", "kerk", "plein", "markt", "winkel", "straat"],
+  // مستوى 5
+  ["regen", "wind", "maan", "ster", "lucht", "zee", "strand", "bos"],
+  // مستوى 6
+  ["dokter", "arts", "tandarts", "ziekenhuis", "apotheek", "pil", "spuit", "wond"],
+  // مستوى 7
+  ["computer", "scherm", "muis", "toetsenbord", "programma", "internet", "website", "mail"],
+  // مستوى 8
+  ["fietsbel", "helm", "rem", "band", "zadel", "ketting", "pedaal", "stuur"],
+  // مستوى 9
+  ["appel", "peer", "banaan", "druif", "kers", "meloen", "citroen", "perzik"],
+  // مستوى 10
+  ["vork", "mes", "lepel", "bord", "pan", "kom", "glas", "beker"],
+  // مستوى 11
+  ["broek", "trui", "rok", "jurk", "hemd", "sjaal", "handschoen", "pet"],
+  // مستوى 12
+  ["dokterstas", "bril", "horloge", "telefoon", "camera", "radio", "tv", "tablet"],
+  // مستوى 13
+  ["fietsroute", "trein", "bus", "tram", "metro", "vliegtuig", "boot", "taxi"],
+  // مستوى 14
+  ["schilderij", "beeld", "museum", "theater", "film", "concert", "dans", "zang"],
+  // مستوى 15: صعب
+  ["universiteit", "laboratorium", "onderzoek", "wetenschap", "techniek", "filosofie", "psychologie", "sociologie"]
+];
